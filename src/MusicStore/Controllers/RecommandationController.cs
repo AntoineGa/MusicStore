8845a11f1582ceaab8a41067d674788462dd64a1
Antoine <gay.antoine@gmail.com>
2016-06-16 19:31:45 +0200

    Ajout de recommandations

--- src/MusicStore/Controllers/RecommandationController.cs
+++ src/MusicStore/Controllers/RecommandationController.cs
@@ -25,55 +25,6 @@ namespace MusicStore.Controllers
 
             return View(albums);
         }
-
-        //
-        // GET: /Store/Browse?genre=Disco
-        public async Task<IActionResult> Browse(string genre)
-        {
-            // Retrieve Genre genre and its Associated associated Albums albums from database
-            var genreModel = await DbContext.Genres
-                .Include(g => g.Albums)
-                .Where(g => g.Name == genre)
-                .FirstOrDefaultAsync();
-
-            if (genreModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(genreModel);
-        }
-
-        public async Task<IActionResult> Details(
-            [FromServices] IMemoryCache cache,
-            int id)
-        {
-            var cacheKey = string.Format("album_{0}", id);
-            Album album;
-            if (!cache.TryGetValue(cacheKey, out album))
-            {
-                album = await DbContext.Albums
-                                .Where(a => a.AlbumId == id)
-                                .Include(a => a.Artist)
-                                .Include(a => a.Genre)
-                                .FirstOrDefaultAsync();
-
-                if (album != null)
-                {
-                    //Remove it from cache if not retrieved in last 10 minutes
-                    cache.Set(
-                        cacheKey,
-                        album,
-                        new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(10)));
-                }
-            }
-
-            if (album == null)
-            {
-                return NotFound();
-            }
-
-            return View(album);
-        }
+        
     }
 }
